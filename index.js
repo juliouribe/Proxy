9c8e8b428a62c4f1500a8ad7558a0a091ec9e3ff
Julio Uribe <uribejulioc@gmail.com>
2023-07-21 14:58:39 -0700

    Setup Proxy

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+
+// Only need dotenv in development mode.
+if (process.env.NODE_ENV !== 'production') {
+  const dotenv = require('dotenv');
+  dotenv.config();
+}
+
+const app = express();
+app.use(cors());
+
+// If we only need to bypass CORS
+app.get('/', async (req, res) => {
+  if (req.query.url) {
+    // Use an & to include multiple query params
+    // process.env.API_KEY
+    const axiosRes = await axios.get(req.query.url)
+    res.send(axiosRes.data);
+  } else {
+    res.send('Please provide a url query param')
+  }
+})
+
+// Tell my app which port to run on.
+app.listen(5001, () => {
+  // Run two servers. One for main project and one for proxy server.
+  console.log('Listening on port 5001...')
+});
+
+
+/*
+
+
+
+*/
+const data = fetch("http://localhost:5001?url=https://google.com");
+
+// Maybe use Darren's proxy host
+// https://cors-proxy-xphi.onrender.com/
+
+// Render . com for hosting
